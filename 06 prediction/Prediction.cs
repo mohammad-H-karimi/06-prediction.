503c27bc534fa20e2ced38c7694531b337674585
mohammad-H-karimi <127038460+mohammad-H-karimi@users.noreply.github.com>
2023-04-17 10:12:26 +0330

    Update Prediction.cs

--- 06 prediction/Prediction.cs
+++ 06 prediction/Prediction.cs
@@ -24,28 +24,38 @@ namespace currency6
             var plus = line1 + line2 + line3 + line4 + line5 + line6;
 
 
-            long average1 = Convert.ToInt32(plus) / 6;
+    
+                long average1 = Convert.ToInt32(plus) / 6;
 
-            var currencyinfo = currencyprocess.currencyinformation();
+                var currencyinfo = currencyprocess.currencyinformation();
 
-            var q = currencyinfo.Result.high;
-            var e = currencyinfo.Result.low;
-            var r = q + e;
-            var t = Convert.ToInt32(r) / 2;
+                var q = currencyinfo.Result.high;
+                var e = currencyinfo.Result.low;
+                var r = q + e;
+                var t = Convert.ToInt32(r) / 2;
 
-            var m = t - average1;
+                var m = t - average1;
 
-            var predictonbuy = m + currencyinfo.Result.buy;
-            var predictonsell = m + currencyinfo.Result.sell;
+                var predictonbuy = m + currencyinfo.Result.buy;
 
 
-            Console.WriteLine("predicton of buy  is:  {0}", predictonbuy);
 
-            Console.WriteLine("predicton of sell  is:  {0}", predictonsell);
+
+                var per = currencyinfo.Result.buy - predictonbuy;
+                var pe = per / currencyinfo.Result.buy;
+                var PE = pe * 100;
+
+
+
+                Console.WriteLine("predicton  is:  {0}", predictonbuy);
+
+                Console.WriteLine(PE);
+
+                Console.WriteLine(currencyinfo.Result.buy + " " + predictonbuy);
+
+            streader.Close();   
+
         }
-
-
-
 
 
     }
